d9fd10761e6f925a1481ad44e2b5c3466462b5f9
cute-android <81049558+cute-android@users.noreply.github.com>
2021-10-20 14:20:18 -0300

    Update collatz.py
    optimized algorithm

--- collatz.py
+++ collatz.py
@@ -10,16 +10,19 @@ while num != 1:
     if num%2 == 0: 
         y.append(int(num/2))
     else:
-        y.append(3*num+1)
+        num = 3*num+1
+        y.append(num)
+        y.append(int(num/2))
+        i+=1
     num = y[i]
-    i = i + 1
+    i+=1
+    
 
 x=[]
 for n in range(i):
     x.append(n+1)
     #Uncomment if you want to see labels
     #plt.text(x[n],y[n]+1, "[ " + str(x[n]) + ", " + str(y[n]) + " ]") 
-
 
 #Uncomment if you want to see the generated sets
 #print(x)
